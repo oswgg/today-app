8deadaa4ff3071d29602d03895f1ea70c05f1b22
oswgg <84472573+oswgg@users.noreply.github.com>
2025-08-02 00:16:54 -0600

    fix: e2e route

--- src/infrastructure/database/prisma.service.ts
+++ src/infrastructure/database/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma';
+import { PrismaClient } from 'src/../generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
